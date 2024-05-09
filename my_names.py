2613cc2935547f2288229f55e3bc6d99b578e08a
azzy <78948500+lAzlinz@users.noreply.github.com>
2024-05-09 10:42:28 +0800

    Create my_names.py
    All the file names and directories used in the program

---
+++ my_names.py
@@ -0,0 +1,30 @@
+directories = {
+    'datasets': './datasets/',
+    'models': './models/',
+    'reports': './reports/'
+}
+
+names_files = {
+    'pattern': 'patterns.csv',
+    'tags': 'tags.csv',
+    'responses': 'responses.csv',
+    'answers': 'answers.json',
+    'test': 'test.json',
+    'train': 'train.json',
+    'eval': 'eval.json',
+    'id2label': 'id2label.json',
+    'label2id': 'label2id.json'
+}
+name_trained_model = 'my_model_recorded_weightedclass_epoch_15'
+
+
+paths = dict()
+for key, name in names_files.items():
+    paths[key] = directories['datasets'] + name
+paths['trained_model'] = directories['models'] + name_trained_model
+paths['pretrained_model'] = 'distilbert/distilbert-base-uncased'
+paths['model_reports'] = directories['reports'] + name_trained_model
+
+if __name__ == '__main__':
+    for _, path in paths.items():
+        print(path)
