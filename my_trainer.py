ced175256100363aaabf63888f31e701914055b9
azzy <78948500+lAzlinz@users.noreply.github.com>
2024-05-19 04:56:23 +0800

    adjusted the loss_fct to be more generalized

--- my_trainer.py
+++ my_trainer.py
@@ -1,9 +1,15 @@
 from transformers import Trainer
 from torch import nn
-import torch, json
+import torch, json, numpy as np
+from my_names import paths
+from sklearn.utils.class_weight import compute_class_weight
 
-with open('./datasets/class_weights.json', 'r') as file:
-    class_weights: list[float] = json.load(file)
+# calculate the class weights
+# get training set
+with open(paths['train'], 'r') as f:
+    train_set = json.load(f)
+    labels = [row['label'] for row in train_set]
+class_weights = compute_class_weight('balanced', classes=np.unique(labels), y=labels)
 
 class CustomTrainer(Trainer):
     def compute_loss (self, model, inputs, return_outputs=False):
@@ -12,6 +18,6 @@ class CustomTrainer(Trainer):
         outputs = model(**inputs)
         logits = outputs.get('logits')
         # compute custom loss
-        loss_fct = nn.CrossEntropyLoss(weight=torch.tensor(class_weights))
+        loss_fct = nn.CrossEntropyLoss(weight=torch.tensor(class_weights).float())
         loss = loss_fct(logits.view(-1, self.model.config.num_labels), labels.view(-1))
         return (loss, outputs) if return_outputs else loss
